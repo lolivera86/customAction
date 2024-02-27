a6c0ddb28a833642b1b65714d4d405655377f1f7
lolivera86 <80928685+lolivera86@users.noreply.github.com>
2024-02-27 13:29:51 +0100

    loop

--- .github/actions/delete_docker_img/ghcr-prune.py
+++ .github/actions/delete_docker_img/ghcr-prune.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         r = s.get(list_url)
         if 'link' in r.headers and 'next' in r.links:
             list_url = r.links['next']['url']
-             print(f'this is the url : {r.links['next']['url']}')
+            print(f'this is the url : {r.links['next']['url']}')
         else:
             list_url = None
 
